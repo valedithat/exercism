ae441f58c864b8d8730cc885562f516b7e8e275c
Valerie Trudell <valedithat@pm.me>
2019-07-23 15:54:44 +0000

    add freeze to variable

--- ruby/resistor-color-duo/resistor_color_duo.rb
+++ ruby/resistor-color-duo/resistor_color_duo.rb
@@ -1,9 +1,7 @@
 class ResistorColorDuo
   def self.value(colors)
-    resistors = { black: 0, brown: 1, red: 2, orange: 3, yellow: 4, green: 5, blue: 6, violet: 7, grey: 8, white: 9 }
+    resistors = { black: 0, brown: 1, red: 2, orange: 3, yellow: 4, green: 5, blue: 6, violet: 7, grey: 8, white: 9 }.freeze
 
-    colors.map do |color|
-      resistors[color.to_sym]
-    end.join.to_i
+    colors.map { |color| resistors[color.to_sym] }.join.to_i
   end
 end
