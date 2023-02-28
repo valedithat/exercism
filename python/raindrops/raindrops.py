3ae5ff1146f1b1168295db4db05bc9203518b488
valedithat <valedithat@pm.me>
2023-02-28 11:52:18 -0500

    add multiple possibilities at once

--- python/raindrops/raindrops.py
+++ python/raindrops/raindrops.py
@@ -1,10 +1,16 @@
 def raindrops(number):
+    result = ''
+
     if number % 3 == 0:
-        result = 'Pling'
-    elif number % 5 == 0:
-        result = 'Plang'
-    elif number % 7 == 0:
-        result = 'Plong'
-    else:
+        result += 'Pling'
+
+    if number % 5 == 0:
+        result += 'Plang'
+
+    if number % 7 == 0:
+        result += 'Plong'
+
+    if result == '':
         result = str(number)
+
     return result
--- python/raindrops/raindrops_test.py
+++ python/raindrops/raindrops_test.py
@@ -16,7 +16,7 @@ class RaindropsTest(unittest.TestCase):
     def test_the_sound_for_6_is_pling_as_it_has_a_factor_3(self):
         self.assertEqual(raindrops(6), "Pling")
 
-    def test_2_to_the_power_3_does_not_make_a_raindrop_sound_as_3_is_the_exponent_not_the_base(self):
+    def test_2_to_the_power_3_does_not_make_a_raindrop_sound(self):
         self.assertEqual(raindrops(8), "8")
 
     def test_the_sound_for_9_is_pling_as_it_has_a_factor_3(self):
@@ -28,20 +28,14 @@ class RaindropsTest(unittest.TestCase):
     def test_the_sound_for_14_is_plong_as_it_has_a_factor_of_7(self):
         self.assertEqual(raindrops(14), "Plong")
 
-    # def test_the_sound_for_15_is_pling_plang_as_it_has_factors_3_and_5(self):
-    #     self.assertEqual(raindrops(15), "PlingPlang")
-
-    # def test_the_sound_for_21_is_pling_plong_as_it_has_factors_3_and_7(self):
-    #     self.assertEqual(raindrops(21), "PlingPlong")
-
     def test_the_sound_for_25_is_plang_as_it_has_a_factor_5(self):
         self.assertEqual(raindrops(25), "Plang")
 
     def test_the_sound_for_27_is_pling_as_it_has_a_factor_3(self):
         self.assertEqual(raindrops(27), "Pling")
 
-    # def test_the_sound_for_35_is_plang_plong_as_it_has_factors_5_and_7(self):
-    #     self.assertEqual(raindrops(35), "PlangPlong")
+    def test_the_sound_for_3125_is_plang_as_it_has_a_factor_5(self):
+        self.assertEqual(raindrops(3125), "Plang")
 
     def test_the_sound_for_49_is_plong_as_it_has_a_factor_7(self):
         self.assertEqual(raindrops(49), "Plong")
@@ -49,11 +43,18 @@ class RaindropsTest(unittest.TestCase):
     def test_the_sound_for_52_is_52(self):
         self.assertEqual(raindrops(52), "52")
 
-    # def test_the_sound_for_105_is_pling_plang_plong_as_it_has_factors_3_5_and_7(self):
-    #     self.assertEqual(raindrops(105), "PlingPlangPlong")
 
-    def test_the_sound_for_3125_is_plang_as_it_has_a_factor_5(self):
-        self.assertEqual(raindrops(3125), "Plang")
+    def test_the_sound_for_35_is_plang_plong_as_it_has_factors_5_and_7(self):
+        self.assertEqual(raindrops(35), "PlangPlong")
+
+    def test_the_sound_for_105_is_pling_plang_plong_as_it_has_factors_3_5_and_7(self):
+        self.assertEqual(raindrops(105), "PlingPlangPlong")
+
+    def test_the_sound_for_15_is_pling_plang_as_it_has_factors_3_and_5(self):
+        self.assertEqual(raindrops(15), "PlingPlang")
+
+    def test_the_sound_for_21_is_pling_plong_as_it_has_factors_3_and_7(self):
+        self.assertEqual(raindrops(21), "PlingPlong")
 
 
 if __name__ == "__main__":
