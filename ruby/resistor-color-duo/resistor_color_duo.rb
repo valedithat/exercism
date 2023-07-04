9222f6bc8dffd02c98cce6eb632e874f68e56087
valedithat <valedithat@pm.me>
2023-07-04 11:29:59 -0400

    complete resistor-color-duo

--- ruby/resistor-color-duo/resistor_color_duo.rb
+++ ruby/resistor-color-duo/resistor_color_duo.rb
@@ -1,7 +1,7 @@
 class ResistorColorDuo
-  def self.value(colors)
-    resistors = { black: 0, brown: 1, red: 2, orange: 3, yellow: 4, green: 5, blue: 6, violet: 7, grey: 8, white: 9 }.freeze
-
-    colors.map { |color| resistors[color.to_sym] }.join.to_i
+  RESISTORS = { black: 0, brown: 1, red: 2, orange: 3, yellow: 4, green: 5, blue: 6, violet: 7, grey: 8, white: 9 }.freeze
+  def self.value(array)
+    colors = array.shift(2)
+    colors.map { |color| RESISTORS[color.to_sym] }.join.to_i
   end
 end
